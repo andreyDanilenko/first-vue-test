9e2a3cc809ca2749901ebf0cc15119fc123c6d41
andreyDanilenko <danilenko.a.g@mail.ru>
2021-11-09 21:29:41 +0300

    slider

--- src/pages/CardPage.vue
+++ src/pages/CardPage.vue
@@ -2,14 +2,41 @@
 .card-page(v-if="card")
   .container 
     h2.card-page__title {{ card.title }}
-    img.card-page__poster(:src="require('../assets/img/' + card.poster)") 
+    p.card-page__price {{ card.price }} $
+    .card-page__info
+      img.card-page__poster(
+        v-if="posterCard !== ''",
+        :src="require('../assets/img/' + posterCard)"
+      ) 
+      img.card-page__poster(
+        v-else,
+        :src="require('../assets/img/' + card.slides[0])"
+      ) 
+      .card-page__slider 
+        button.card-page__slide(
+          @click="opened(index)",
+          v-for="(item, index) in card.slides",
+          :key="index"
+        )
+          img.card-page__slide-img(:src="require('../assets/img/' + item)")
+      p.card-page__description {{ card.description }}
 </template>
 <script>
 import { mapActions, mapState, mapMutations } from "vuex";
 export default {
+  data() {
+    return {
+      posterCard: "",
+    };
+  },
+
   methods: {
     ...mapActions(["GET_CARD_FROM_API"]),
     ...mapMutations(["SET_CARD_DESTROY"]),
+
+    opened(evt) {
+      this.posterCard = this.card.slides[evt];
+    },
   },
 
   created() {
@@ -36,10 +63,47 @@ export default {
     text-align: center;
   }
 
-  &__poster {
-    margin: 40px auto 0;
+  &__info {
     width: 50%;
+    margin: 40px auto 0;
+    border: 1px solid #e1e1e1;
+    padding: 20px;
+  }
+
+  &__poster {
+    width: 100%;
     display: block;
+  }
+
+  &__description {
+    margin-top: 20px;
+    text-align: center;
+  }
+
+  &__slider {
+    display: flex;
+  }
+
+  &__slide {
+    padding: 10px;
+    cursor: pointer;
+    background: transparent;
+    border: 1px solid #e1e1e1;
+    margin: 5px;
+
+    img {
+      display: block;
+      width: 100%;
+    }
+  }
+
+  &__price {
+    width: 50%;
+    font-size: 40px;
+    text-align: center;
+    margin: 40px auto 0;
+    border: 1px solid #e1e1e1;
+    padding: 20px;
   }
 }
 </style>
